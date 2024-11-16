5075690c62bc7fabf723b0e56ec00a3f0ebd74e2
GustavMolin <gustav@scandicsource.com>
2024-11-16 21:59:42 +0100

    Add files via upload

---
+++ controllers/image.js
@@ -0,0 +1,28 @@
+const Clarifai = require('clarifai');
+
+const app = new Clarifai.App({
+    apiKey: '590fb7c8084c487d875baba6b32cc2d5'
+});
+
+const handleApiCall = (req, res) => {
+app.models
+.predict(Clarifai.FACE_DETECT_MODEL, req.body.input)
+.then(data => {
+    res.json(data);
+})
+.catch(err => res.status(400).json('buhuuuuu'))}
+const handleImage = (req, res, db) => {
+    const { id } = req.body;
+    db('users').where('id', '=', id)
+        .increment('entries', 1)
+        .returning('entries')
+        .then(entries => {
+            res.json(entries[0].entries);
+        })
+        .catch(err => res.status(400).json('unable to get entries'))
+}
+
+module.exports = {
+    handleImage,
+    handleApiCall
+}
